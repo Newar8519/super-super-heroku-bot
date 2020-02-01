9ce9af505d9911ab4a25723b20c7c993f5c2fe14
Newar8519 <58194569+Newar8519@users.noreply.github.com>
2020-02-01 09:25:50 -0500

    Update rules.js

--- bot-command/rules.js
+++ bot-command/rules.js
@@ -1,10 +1,18 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, RichEmbed } = require('discord.js');
+const client = new Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
 
 client.on('message', message => {
-    if (message.content === '?Rules') {
-    	message.channel.send('True!');
-  	}
+  if (message.content === '?Help') {
+    const embed = new RichEmbed()
+      .setTitle('Support Command!')
+      .setColor(0xFF0000)
+      .setDescription('This works if you see this.');
+    message.channel.send(embed);
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
