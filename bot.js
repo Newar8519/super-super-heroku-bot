99571573aa27ed780ec2695284ec9b3e266d9cfb
Newar8519 <58194569+Newar8519@users.noreply.github.com>
2020-02-01 10:24:07 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,14 +3,11 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-
-    avatar = "https://cdn.discordapp.com/attachments/663132724181073942/672994269270900776/1202399791_preview_g4uJDxhZu4o.jpg";
-    client.user.setAvatar (avatar);
 });
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	return message.channel.reply({embed: {
+    	message.channel.reply({embed: {
         color: 3447003,
         description: "This bot was programed by me"
         }});
