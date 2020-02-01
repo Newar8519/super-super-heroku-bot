2a88ed14be90112b37ab36d0d5f92ee4a3abc1ce
Newar8519 <58194569+Newar8519@users.noreply.github.com>
2020-01-31 21:39:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,9 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+
+    avatar = "https://cdn.discordapp.com/attachments/663132724181073942/672994269270900776/1202399791_preview_g4uJDxhZu4o.jpg";
+    client.user.setAvatar (avatar);
 });
 
 client.on('message', message => {
