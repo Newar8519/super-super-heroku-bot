e4af77c687adbce71d15403222c288715c594526
Newar8519 <58194569+Newar8519@users.noreply.github.com>
2020-02-01 08:57:39 -0500

    Create rules.js

---
+++ bot-command/rules.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('message', message => {
+    if (message.content === 'Rules') {
+    	message.channel.send('pong\n True!');
+  	}
+});
+
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
